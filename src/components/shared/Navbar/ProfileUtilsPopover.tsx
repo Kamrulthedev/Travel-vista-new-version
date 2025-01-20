4089382ba20810089421362f9a01c8095695cb39
Kamrul Hassan <kamrulthedev@gmail.com>
2025-01-20 09:32:48 +0600

    update-navber-commit

--- src/components/shared/Navbar/ProfileUtilsPopover.tsx
+++ src/components/shared/Navbar/ProfileUtilsPopover.tsx
@@ -68,72 +68,74 @@ export function ProfileUtilsPopover() {
         </button>
       </PopoverTrigger>
       <PopoverContent className="popover-content w-fit" align="start">
-        <ul className="space-y-2">
-          <li>
-            <div className="flex items-center gap-x-2">
-              <Image
-                src={userData?.profileImage || "/icons/avatar.png"}
-                width={40}
-                height={40}
-                alt={userData?.name || "avatar"}
-                className="rounded-full bg-slate-300 size-12 p-1 object-cover"
-              />
-              <aside>
-                <p className="font-medium flex items-center gap-1">
-                  {userData?.name}{" "}
-                  {userData?.status === "Premium" && (
-                    <PremiumUserToolTip iconSize={14} />
-                  )}{" "}
-                </p>
-                <p className="text-gray-500 text-xs">Blogger</p>
-              </aside>
-            </div>
-            <Link
-              href={`/profile/${userData?._id}`}
+        <div>
+          <ul className="space-y-2">
+            <li>
+              <div className="flex items-center gap-x-2">
+                <Image
+                  src={userData?.profileImage || "/icons/avatar.png"}
+                  width={40}
+                  height={40}
+                  alt={userData?.name || "avatar"}
+                  className="rounded-full bg-slate-300 size-12 p-1 object-cover"
+                />
+                <aside>
+                  <p className="font-medium flex items-center gap-1">
+                    {userData?.name}{" "}
+                    {userData?.status === "Premium" && (
+                      <PremiumUserToolTip iconSize={14} />
+                    )}
+                  </p>
+                  <p className="text-gray-500 text-xs">Blogger</p>
+                </aside>
+              </div>
+              <Link href={`/profile/${userData?._id}`} legacyBehavior>
+                <button
+                  onClick={() => setOpen(false)}
+                  className="bg-blue-100 text-primary px-4 py-1 rounded-sm w-full mt-2 hover:bg-primary hover:text-white transition-colors outline-none"
+                >
+                  Profile
+                </button>
+              </Link>
+            </li>
+            {listItems.map((listItem) => (
+              <li
+                key={listItem.id}
+                className="flex items-start gap-x-2 text-gray-700 w-full cursor-pointer p-1"
+                onClick={() => setOpen(false)} // Close the popover on item click
+              >
+                {listItem.icon}
+                <span className="hover:text-primary transition-colors text-sm">
+                  {listItem.title}
+                </span>
+              </li>
+            ))}
+            <li
+              className="border-y border-gray-300 flex items-start gap-x-2 text-gray-700 w-full cursor-pointer py-3"
               onClick={() => setOpen(false)}
             >
-              <button className="bg-blue-100 text-primary px-4 py-1 rounded-sm w-full mt-2 hover:bg-primary hover:text-white transition-colors outline-none">
-                Profile
+              <Power size={18} />
+              <button
+                onClick={handleLogout}
+                className="hover:text-primary transition-colors text-sm"
+              >
+                Sign Out
               </button>
-            </Link>
-          </li>
-          {listItems.map((listItem) => (
-            <li
-              key={listItem.id}
-              className="flex items-start gap-x-2 text-gray-700 w-full cursor-pointer p-1"
-              onClick={() => setOpen(false)} // Close the popover on item click
-            >
-              {listItem.icon}
-              <span className="hover:text-primary transition-colors text-sm">
-                {listItem.title}
-              </span>
             </li>
-          ))}
-          <li
-            className="border-y border-gray-300 flex items-start gap-x-2 text-gray-700 w-full cursor-pointer py-3"
-            onClick={() => setOpen(false)}
-          >
-            <Power size={18} />
-            <button
-              onClick={handleLogout}
-              className="hover:text-primary transition-colors text-sm"
-            >
-              Sign Out
-            </button>
-          </li>
-          <li className="flex items-center justify-between gap-x-2">
-            Mode:
-            <button className="bg-primary text-white px-2 py-1 rounded-sm">
-              <Sun size={18} />
-            </button>
-            <button className="hover:bg-primary hover:text-white px-2 py-1 rounded-sm transition-colors">
-              <Moon size={18} />
-            </button>
-            <button className="hover:bg-primary hover:text-white px-2 py-1 rounded-sm transition-colors">
-              <SunMoon size={18} />
-            </button>
-          </li>
-        </ul>
+            <li className="flex items-center justify-between gap-x-2">
+              Mode:
+              <button className="bg-primary text-white px-2 py-1 rounded-sm">
+                <Sun size={18} />
+              </button>
+              <button className="hover:bg-primary hover:text-white px-2 py-1 rounded-sm transition-colors">
+                <Moon size={18} />
+              </button>
+              <button className="hover:bg-primary hover:text-white px-2 py-1 rounded-sm transition-colors">
+                <SunMoon size={18} />
+              </button>
+            </li>
+          </ul>
+        </div>
       </PopoverContent>
     </Popover>
   );
