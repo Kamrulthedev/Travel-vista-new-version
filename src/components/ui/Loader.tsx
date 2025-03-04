6e004652bf4a89d0730bcb555f7df3729252a7c1
Kamrul Hassan <kamrulthedev@gmail.com>
2025-03-04 19:24:20 +0600

    update-code

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,14 +1,13 @@
 import "./LoaderStyles.css";
 const Loader = () => {
   return (
-    <div className="absolute inset-0 w-full h-full flex justify-center items-center z-[9999] bg-gray-100">
-    <div className="spinners">
-      <div className="spinner"></div>
-      <div className="spinner delay-1"></div>
-      <div className="spinner delay-2"></div>
+    <div className="loader-container">
+      <div className="spinners">
+        <div className="spinner"></div>
+        <div className="spinner delay-1"></div>
+        <div className="spinner delay-2"></div>
+      </div>
     </div>
-  </div>
   );
 };
-
 export default Loader;
