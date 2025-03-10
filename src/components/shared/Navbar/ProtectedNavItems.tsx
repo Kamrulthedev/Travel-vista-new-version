56b5cb0c99e3853737896e807a9c992d1334c22b
Kamrul Hassan <kamrulthedev@gmail.com>
2025-03-10 15:27:53 +0600

    navber-button update

--- src/components/shared/Navbar/ProtectedNavItems.tsx
+++ src/components/shared/Navbar/ProtectedNavItems.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { SignInFormModal } from "@/app/_authentication/SignInFormModal";
 import { useUser } from "@/context/userProvider";
-import { Bell, Contact, House, Info, MessageSquareText } from "lucide-react";
+import { Bell, Contact, House, Info, LayoutDashboard, MessageSquareText } from "lucide-react";
 import Link from "next/link";
 import { ProfileUtilsPopover } from "./ProfileUtilsPopover";
 
@@ -11,7 +11,7 @@ const ProtectedNavItems = () => {
   const navItems = [
     {
       id: 3,
-      title: "Dashboard",
+      title: "",
       path: "/dashboard",
       isPrivate: true,
     },
@@ -25,39 +25,40 @@ const ProtectedNavItems = () => {
   return user ? (
     <>
       <Link href={"/"}>
-        <li className="w-9 h-9 rounded-sm flex justify-center items-center bg-gray-200 cursor-pointer">
-          <House size={18} strokeWidth={1} />
+        <li className="w-9 h-9 rounded-sm flex justify-center items-center hover:bg-gray-200 cursor-pointer">
+          <House size={18} strokeWidth={2} className="text-black"/>
         </li>
       </Link>
 
-      <li className="w-20 h-9 rounded-sm flex items-center justify-center bg-gray-200 cursor-pointer px-2">
-        <Link href="/about-us" className="flex items-center gap-1 text-sm">
-         <Info strokeWidth={1} size={16} />
+      <li className="w-9 h-9 rounded-sm flex items-center justify-center hover:bg-gray-200 cursor-pointer">
+        <Link href="/about-us">
+         <Info  strokeWidth={2}  size={18} className="text-black"/>
         </Link>
       </li>
 
-      <li className="w-20 h-9 rounded-sm flex items-center justify-center bg-gray-200 cursor-pointer px-2">
-        <Link href="/" className="flex items-center gap-1 text-sm">
-          <MessageSquareText strokeWidth={1} size={16} />
+      <li className="w-9 h-9 rounded-sm flex items-center justify-center hover:bg-gray-200 cursor-pointer px-2">
+        <Link href="/">
+          <MessageSquareText  strokeWidth={2}  size={18} className="text-black" />
         </Link>
       </li>
 
-      <li className="w-20 h-9 rounded-sm flex items-center justify-center bg-gray-200 cursor-pointer relative px-2">
-        <Link href="/" className="flex items-center gap-1 text-sm">
-          <Bell strokeWidth={1} size={16} />
+      <li className="w-9 h-9 rounded-sm flex items-center justify-center hover:bg-gray-200 cursor-pointer relative px-2">
+        <Link href="/">
+          <Bell  strokeWidth={2}  size={18} className="text-black" />
         </Link>
         <span className="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full transition-all duration-500 ease-in-out"></span>
       </li>
 
-      <li className="w-20 h-9 rounded-sm flex items-center justify-center bg-gray-200 cursor-pointer px-2">
+      <li className="w-9 h-9 rounded-sm flex items-center justify-center hover:bg-gray-200 cursor-pointer px-2">
         <Link href="/contact-us" className="flex items-center gap-1 text-sm">
-           <Contact strokeWidth={1} size={16} />
+           <Contact  strokeWidth={2}  size={18} className="text-black" />
         </Link>
       </li>
 
       {navItems.map((navItem) => (
-        <li key={navItem.id} className="w-28 h-9 rounded-sm flex items-center justify-center bg-gray-200 cursor-pointer px-2">
+        <li key={navItem.id} className="w-9 h-9 rounded-sm flex items-center justify-center hover:bg-gray-200 cursor-pointer px-2">
           <Link href={navItem.path} className="text-sm">
+            <LayoutDashboard></LayoutDashboard>
             {navItem.title}
           </Link>
         </li>
